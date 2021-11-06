999ae20bb1275c72a1090f75caf19ae185011ce2
giscafer <giscafer@outlook.com>
2021-11-06 23:52:33 +0800

    release(test): 删除ora

--- scripts/upload-file.js
+++ scripts/upload-file.js
@@ -1,17 +1,14 @@
 const request = require('request');
 const chalk = require('chalk');
-const ora = require('ora');
 const { createReadStream } = require('fs');
 const UPLOAD_URL = process.env.UPLOAD_URL;
 const UPLOAD_TOKEN_URL = process.env.UPLOAD_TOKEN_URL;
-const bootSpinner = ora(
-  `Creating ${chalk.bold.green('获取file upload token')}...`
-);
+
 function requestWebhook(opt, cb) {
   return request.post(UPLOAD_URL, { ...opt }, (error, response, body) => {
     if (error) {
       console.log(error);
-      bootSpinner.fail(`${chalk.bold.red('附件上传失败')}`);
+      console.log(`${chalk.bold.red('附件上传失败')}`);
       return;
     }
     // console.log(body);
@@ -20,7 +17,7 @@ function requestWebhook(opt, cb) {
       cb(result.cosPath);
     }
     // console.log('附件发送完成', result.cosPath);
-    bootSpinner.succeed(
+    console.log(
       `${chalk.bold.green('附件上传完成！')}，地址为: ${result.cosPath}`
     );
   });
@@ -37,7 +34,7 @@ function sendFile(filename, filePath, cb) {
     const fileUploadToken = result.data?.fileUploadToken;
     console.log('fileUploadToken=', fileUploadToken);
 
-    bootSpinner.start(`${chalk.bold.green('附件上传服务器')}...`);
+    console.log(`${chalk.bold.green('附件上传服务器')}...`);
     const r = requestWebhook(
       {
         headers: {
