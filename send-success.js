3fd57d0e100aec11d2ceb671801b8d0a5936fc04
giscafer <giscafer@outlook.com>
2021-11-08 23:27:26 +0800

    dev

--- send-success.js
+++ send-success.js
@@ -1,6 +1,7 @@
 const { notificationSuccess } = require('./notification');
-notificationSuccess({
-  msgtype: 'text',
-  content:
-    '构建apk已上传Artifact：https://github.com/RootLinkFE/AutoBuild-React-Native/actions',
-});
+const BUILD_URL =
+  process.argv[2] ||
+  'https://github.com/RootLinkFE/AutoBuild-React-Native/actions';
+
+const context = `Android构建成功\n>已上传Github Artifact: ${BUILD_URL}\n>正上传到服务器生成二维码（可能会超时）……`;
+notificationSuccess(context, () => {});
