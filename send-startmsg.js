fa07e17cadc563744e7ef1fc97cca47a29271b24
giscafer <giscafer@outlook.com>
2021-11-08 23:31:57 +0800

    [skip ci]

---
+++ send-startmsg.js
@@ -0,0 +1,7 @@
+const { notificationSuccess } = require('./notification');
+const BUILD_URL =
+  process.argv[2] ||
+  'https://github.com/RootLinkFE/AutoBuild-React-Native/actions';
+
+const context = `>Android正在构建: ${BUILD_URL}`;
+notificationSuccess(context, () => {});
