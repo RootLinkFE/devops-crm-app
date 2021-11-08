2a100d16ad46426c61417465fbefdff267f049c5
giscafer <giscafer@outlook.com>
2021-11-08 22:26:47 +0800

    dev

--- send-error.js
+++ send-error.js
@@ -1,6 +1,6 @@
 const { notificationFailed } = require('./notification');
 
-notificationFailed(
-  'https://github.com/RootLinkFE/AutoBuild-React-Native/actions',
-  'Android打包失败'
-);
+// notificationFailed(
+//   'https://github.com/RootLinkFE/AutoBuild-React-Native/actions',
+//   'Android打包失败'
+// );
