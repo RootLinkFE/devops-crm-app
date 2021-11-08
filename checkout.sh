89d0bb21475fd51a14a587130e77ab7be52cbcc5
giscafer <giscafer@outlook.com>
2021-11-08 11:33:30 +0800

    chore: [test]

--- checkout.sh
+++ checkout.sh
@@ -4,6 +4,10 @@
 set -e
 
 branchName=${1}
+dev='[dev]'
+test='[test]'
+master='[master]'
+prod='[prod]'
 
 function log() {
   echo "$(date)>>>>$@"
@@ -11,27 +15,40 @@ function log() {
 
 repositoryUrl="${GITLAB_REPO_URL}"
 
-log "${commitmsg}"
-log $repositoryUrl
-log $branchName
-
-pwd
-
+# 克隆代码
 git clone  $repositoryUrl
 
+
+# 切换分支
+log "${commitmsg}"
 cd g-crm-app 
 
-git checkout $branchName
+if [[ $commitmsg == *$test* ]];then
+    echo "包含[test]"
+    git checkout test
+elif [[ $commitmsg == *$master* ]];then
+    echo "包含[master]"
+    git checkout master
+elif [[ $commitmsg == *$prod* ]];then
+    echo "包含[prod]"
+    git checkout prod
+else
+    echo "包含[dev]"
+    git checkout dev
+fi
+
 
 log "$(git branch)"
-
+# 拉取最新代码
 git pull
 
 cd ..  
-
+# 将代码放到github runner 执行目录下
 cd g-crm-app && mv * ../ 
 
 pwd
 
 ls -l
 
+# 此处应该有切换环境服务地址的脚本执行
+# node ./scripts/prebuild.js
