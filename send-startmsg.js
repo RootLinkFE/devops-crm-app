0ff95dd02eefc96d229faa873c1272aa02396f59
giscafer <giscafer@outlook.com>
2021-11-09 14:14:55 +0800

    [skip ci]

--- send-startmsg.js
+++ send-startmsg.js
@@ -3,5 +3,13 @@ const BUILD_URL =
   process.argv[2] ||
   'https://github.com/RootLinkFE/AutoBuild-React-Native/actions';
 
-const context = `>Android正在构建: [${BUILD_URL}](${BUILD_URL})`;
+const uploadCloud = process.argv[3];
+let text = '';
+if (uploadCloud === 'true') {
+  text = '\n>选择了上传公司文件服务器';
+} else {
+  text = '\n>**未选择**上传公司文件服务器';
+}
+
+const context = `>Android正在构建: [${BUILD_URL}](${BUILD_URL})${text}`;
 notificationSuccess(context, () => {});
