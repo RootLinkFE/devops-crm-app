f53eea8e80f790d5cd517fdff87c8df3b4105bc6
giscafer <giscafer@outlook.com>
2021-11-10 14:00:43 +0800

    [skip ci]

--- send-startmsg.js
+++ send-startmsg.js
@@ -6,14 +6,10 @@ const BUILD_URL =
 const uploadCloud = process.argv[3];
 const platform = process.argv[4] || 'Android';
 let text = '';
-if (uploadCloud === 'true' && platform.toLowerCase() === 'android') {
-  text = '\n>选择了上传公司文件服务器';
-} else if (uploadCloud !== 'true' && platform.toLowerCase() === 'android') {
-  text = '\n>**未选择**上传公司文件服务器';
-}
-if (uploadCloud === 'true' && platform.toLowerCase() === 'ios') {
-  text = '\n>选择了同步到蒲公英平台';
-} else if (uploadCloud !== 'true' && platform.toLowerCase() === 'ios') {
+
+if (uploadCloud === 'true') {
+  text = '\n>选择了**同步**到蒲公英平台';
+} else if (uploadCloud !== 'true') {
   text = '\n>选择**不同步**到蒲公英平台';
 }
 
