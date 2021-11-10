0a477f12196e95f176c6f81b9755a617cb512e78
giscafer <giscafer@outlook.com>
2021-11-10 15:14:23 +0800

    chore: 修改构建成功提示

--- send-success.js
+++ send-success.js
@@ -5,10 +5,8 @@ const BUILD_URL =
 const uploadCloud = process.argv[3];
 const platform = process.argv[4] || 'Android';
 let text = '';
+
 if (uploadCloud === 'true') {
-  text = '\n>正上传到服务器生成二维码（Github走国内服务器可能会超时）……';
-}
-if (uploadCloud === 'true' && platform.toLowerCase() === 'ios') {
   text = '\n>正在同步到蒲公英平台……';
 }
 const context = `${platform}构建成功🎉 \n>已上传Github Artifact，[点此下载(需登陆)](${BUILD_URL})${text}`;
